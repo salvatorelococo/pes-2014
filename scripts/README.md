c48dcc8884a1ceb721fc32e6d27058f5aaa9a24b
Salvatore Lo Coco <salvatore.lococo.slc@gmail.com>
2021-07-19 03:54:53 +0200

    Added multi-unzlib.py

--- scripts/README.md
+++ scripts/README.md
@@ -4,7 +4,8 @@ Here you will find the **code**.
 
 filename            | description
 --------------------|-------------
+multi_unzlib.py     | Tool to extract uncompressed content from all zlib files in a folder. Adapted for PES headers.
 names_scraper.py    | Inspects the ID00015 file and generate a .csv file with team names, team abbreviations and their position.
+names_structure.py  | Contains info about the structure of ID00015.
 names_updater.py    | Using an edited version of a .csv generated with names_scraper.py this script updates the ID00015 file. 
 teams_scraper.py    | Inspects the ID00015 file and generates two .csv files: one containing info about nationalities and another one containing info about teams.
-names_structure.py  | Contains info about the structure of ID00015.
---
+++ scripts/multi-unzlib.py
@@ -0,0 +1,55 @@
+import zlib
+import os
+import time
+
+MAX_OFFSET = 512
+
+
+def main():
+    files = [x for x in os.listdir() if os.path.isfile(x)]
+    timestamp = time.time()
+    os.mkdir(str(timestamp))
+
+    for file in files:
+        print(file)
+        with open(file, 'rb') as f:
+            try:
+                stream = f.read()
+                counter = 0
+                offset = 0
+
+                while True:
+                    dco = zlib.decompressobj()
+
+                    try:
+                        dec = dco.decompress(stream)
+
+                        if not dec:
+                            break
+                    except:
+                        offset += 1
+
+                        if stream == b'' or offset > MAX_OFFSET:
+                            break
+
+                        stream = stream[1:]
+                        continue
+
+                    offset = 0
+
+                    with open(f'{timestamp}/{file}.unzlib_{counter:03}', 'wb+') as o:
+                        o.write(dec)
+                        counter += 1
+
+                    stream = dco.unused_data
+
+                    if not stream:
+                        break
+
+            except Exception as e:
+                print(e.__str__())
+
+
+if __name__ == '__main__':
+    main()
+    input()
